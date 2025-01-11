d56f6f7f365b9db013fc6286c930c1cf4984e74a
aydincandan <aydincandan@gmail.com>
2025-01-11 15:12:53 +0300

    modüller değişti

--- nodecdnApp.js
+++ nodecdnApp.js
@@ -1,70 +1,33 @@
-const cluster = require('cluster');
-
-
-// const { express, app, server, port, catchallroute } = require('../Back-End-NodeMiddle-npmjs.com/_npmjs/express-layer')
-// .parametre({filename:__filename})
-// server.on('listening', () => {
-//   app.get('/', (req, res) => { res.send('Express.js uygulamasi calisiyor. ' + port) });
-//   app.all('*', catchallroute); console.log("ok")
-// });
-
-
-
-
-
-const express = require('express');
-const app = express();
-app.listen(3001, () => { console.log("app listen 3001") });
-// bunu mu kullanayım 
-// yoksa 
-// aşağıdakini mi?  Hangisi daha doğru.  Ayrı ayrı denedim ikiside çalışıyor? CEVAP : https://chatgpt.com/share/676fdcb9-353c-8011-93ca-bc7b28b0b787
-// const express = require('express');
-// const app = express();
-// const http = require('http');
-// const server = http.createServer(app);
-// server.listen(3001, () => { console.log("server listen 3001") });
-
-
-const path = require('path');
-const fs = require('fs');
-const crypto = require('crypto');
-
-const UglifyJS = require("uglify-js");
-
-var mycors = require('mycorssets').seturls([]);
-app.use(mycors.corsWithwhitelist);
-
-// // = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
-// // = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
-// app.get("/nodecdn/MandrakeModule.js", async (req, res) => {
-//   // https://chatgpt.com/share/67533c08-9eac-8011-9160-aa432b147558
-
-//   // try {
-//   // Dosyanın tam yolunu belirtin
-//   const filePath = path.join(__dirname, "../back-end-nodemiddle/MandrakeModule.js");
-//   console.log(filePath)
-//   res.status(200).sendFile(filePath);
-//   // } catch (error) {
-//   //   console.log("MandrakeModule.js Dosya gönderiminde hata:", error);
-//   //   res.status(500).send("Dosya sunulurken, bir hata oluştu.");
-//   // }
-// });
-// // = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
-// // = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
-app.get("/rediscdn/MandrakeModule.js", async (req, res) => {
-  const uzakfilePath = path.join(__dirname, "../back-end-nodemiddle/ClientServerModules/MandrakeModule.js");
-  const yerelfilePath = path.join(__dirname, "./MandrakeModule.js");
-  sikistir_IfChanged_save(uzakfilePath, yerelfilePath)
-  console.log(yerelfilePath)// production için atılması(okunması) gereken bu yerelfilePath
-  res.status(200).sendFile(yerelfilePath);
-});
-app.get("/rediscdn/XanaduModule.js", async (req, res) => {
-  const uzakfilePath = path.join(__dirname, "../back-end-nodemiddle/ClientServerModules/XanaduModule.js");
-  const yerelfilePath = path.join(__dirname, "./XanaduModule.js");
-  sikistir_IfChanged_save(uzakfilePath, yerelfilePath)
-  console.log(yerelfilePath)// production için atılması(okunması) gereken bu yerelfilePath
-  res.status(200).sendFile(yerelfilePath);
-});
+const selftest = () => {
+  // ------------- https://github.com/expressjs/multer/issues/1176 ------------
+  const boundary = 'AaB03x'
+  const body = [
+    '--' + boundary,
+    'Content-Disposition: form-data; name="file"; filename="test.txt"',
+    'Content-Type: text/plain',
+    '',
+    'test without end boundary'
+  ].join('\r\n')
+  const options = {
+    hostname: 'localhost',
+    port,
+    path: '/upload',
+    method: 'POST',
+    headers: {
+      'content-type': 'multipart/form-data; boundary=' + boundary,
+      'content-length': body.length,
+    }
+  }
+  const req = http.request(options, (res) => {
+    console.log(res.statusCode)
+  })
+  req.on('error', (err) => {
+    console.log(err)
+  })
+  req.write(body)
+  req.end()
+  // ------------- https://github.com/expressjs/multer/issues/1176 ------------
+}
 // = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
 // = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
 const sikistir_IfChanged_save = (uzakfilePath, yerelfilePath) => {
@@ -125,6 +88,91 @@ const sikistir_IfChanged_save = (uzakfilePath, yerelfilePath) => {
 }
 // = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
 // = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
+
+
+
+
+
+const cluster = require('cluster');
+
+
+// const { express, app, server, port, catchallroute } = require('../Back-End-NodeMiddle-npmjs.com/_npmjs/express-layer')
+// .parametre({filename:__filename})
+// server.on('listening', () => {
+//   app.get('/', (req, res) => { res.send('Express.js uygulamasi calisiyor. ' + port) });
+//   app.all('*', catchallroute); console.log("ok")
+// });
+
+
+
+
+
+const express = require('express');
+const app = express();
+app.listen(3001, () => { console.log("app listen 3001") });
+// bunu mu kullanayım 
+// yoksa 
+// aşağıdakini mi?  Hangisi daha doğru.  Ayrı ayrı denedim ikiside çalışıyor? CEVAP : https://chatgpt.com/share/676fdcb9-353c-8011-93ca-bc7b28b0b787
+// const express = require('express');
+// const app = express();
+// const http = require('http');
+// const server = http.createServer(app);
+// server.listen(3001, () => { console.log("server listen 3001") });
+
+
+const path = require('path');
+const fs = require('fs');
+const crypto = require('crypto');
+
+const UglifyJS = require("uglify-js");
+
+var mycors = require('mycorssets').seturls([]);
+app.use(mycors.corsWithwhitelist);
+
+// // = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
+// // = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
+// app.get("/nodecdn/MandrakeModule.js", async (req, res) => {
+//   // https://chatgpt.com/share/67533c08-9eac-8011-9160-aa432b147558
+
+//   // try {
+//   // Dosyanın tam yolunu belirtin
+//   const filePath = path.join(__dirname, "../back-end-nodemiddle/MandrakeModule.js");
+//   console.log(filePath)
+//   res.status(200).sendFile(filePath);
+//   // } catch (error) {
+//   //   console.log("MandrakeModule.js Dosya gönderiminde hata:", error);
+//   //   res.status(500).send("Dosya sunulurken, bir hata oluştu.");
+//   // }
+// });
+// // = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
+// // = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
+
+const updateMandrakeModule = () => {
+  const uzakfilePath = path.join(__dirname, "../back-end-nodemiddle/ClientServerModules/MandrakeModule.js");
+  const yerelfilePath = path.join(__dirname, "./MandrakeModule.js");
+  sikistir_IfChanged_save(uzakfilePath, yerelfilePath)
+  // console.log(yerelfilePath)// production için atılması(okunması) gereken bu yerelfilePath
+  console.log("updateMandrakeModule ÇALIŞTI")
+  return yerelfilePath
+}
+const updateXanaduModule = () => {
+  const uzakfilePath = path.join(__dirname, "../back-end-nodemiddle/ClientServerModules/XanaduModule.js");
+  const yerelfilePath = path.join(__dirname, "./XanaduModule.js");
+  sikistir_IfChanged_save(uzakfilePath, yerelfilePath)
+  // console.log(yerelfilePath)// production için atılması(okunması) gereken bu yerelfilePath
+  console.log("updateXanaduModule ÇALIŞTI")
+  return yerelfilePath
+}
+
+updateMandrakeModule();
+updateXanaduModule();
+
+app.get("/rediscdn/MandrakeModule.js", async (req, res) => {
+  res.status(200).sendFile(updateMandrakeModule());
+});
+app.get("/rediscdn/XanaduModule.js", async (req, res) => {
+  res.status(200).sendFile(updateXanaduModule());
+});
 
 
 // https://chatgpt.com/share/67541ae7-8c48-8011-afe4-5d40bc2eb436
